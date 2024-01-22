172882a80b54456fef99c7df8a0f71f8471f76f9
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2024-01-22 16:23:35 +1300

    refactor: refactor code of the '03-files-in-folder' to unify the style

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,29 @@
-const path = require("path");
-const fs = require("fs");
+const { join, extname, basename } = require("node:path");
+const { readdir, stat } = require("node:fs");
+const { stdout } = require("node:process");
 
-const folder_path = path.join(__dirname, "secret-folder");
+const folder_path = join(__dirname, "secret-folder");
 
-fs.readdir(folder_path, {withFileTypes: true}, function(err, files) {
+readdir(folder_path, {withFileTypes: true}, function(err, files) {
+  if (err) {
+    stdout.write("Something is wrong with the 'readdir' method");
+  }
   for (let file of files) {
-    const file_path = path.join(folder_path, file.name);
+    const file_path = join(folder_path, file.name);
 
-    const extension = (file.name[0] !== ".") ? path.extname(file.name) :
-      (path.extname(file.name.slice(1)) === true) ? path.extname(file.name.slice(1)) : "";
+    const extension = (file.name[0] !== ".") ? extname(file.name) :
+      (extname(file.name.slice(1)) === true) ? extname(file.name.slice(1)) : "";
     const output_extension = extension.slice(1);
-    const output_name = path.basename(file.name, extension);
+    const output_name = basename(file.name, extension);
 
-    fs.stat(file_path, function(err, stats) {
+    stat(file_path, function(err, stats) {
+      if (err) {
+        stdout.write("Something is wrong with the 'stat' method");
+      }
       if (stats.isFile()) {
         const output_size = stats.size + " b";
         const output = `${output_name} - ${output_extension} - ${output_size}`;
-        console.log(output);
+        stdout.write(output + "\n");
       }
     });
   }
