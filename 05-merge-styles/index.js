dce99e506a8a39f94a5b28a4562d89896c23d3f6
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2024-01-23 12:20:28 +1300

    feat: add solution for '05-merge-styles' task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,35 +1,42 @@
 const { join, extname } = require("node:path");
-const { readdir } = require("node:fs/promises");
+const { readdir, writeFile } = require("node:fs/promises");
 const { createReadStream, createWriteStream } = require("node:fs");
-const { createInterface } = require("node:readline");
 
 const output_file = join(__dirname, "project-dist", "bundle.css");
 
-async function copyFileContent(path) {
+async function mergeSingleFile(path) {
   const read_stream = createReadStream(path, "utf8");
-  const write_stream = createWriteStream(output_file, "utf8");
-  const readline = createInterface(read_stream, write_stream);
-  readline.on("line", function(line) {
-    write_stream.write(line + "\n");
-  }); 
-  readline.on("close", function() {
-    process.exit();
-  });
+  const write_stream = createWriteStream(output_file, {flags: "a"}, "utf8");
+  read_stream.pipe(write_stream);
+  //console.log("mergeFile completed");
 }
 
-/*async function sortFiles() {
+async function clearBundle(path) {
+  await writeFile(path, "", "utf8");
+}
+
+async function createBundle() {
   const source_folder = join(__dirname, "styles");
-  let styles = [];
+  const project_folder = join(__dirname, "project-dist");
+
+  const project_files = await readdir(project_folder, {withFileTypes: true});
+  for (const file of project_files) {
+    if (file.isFile() === true && file.name === "bundle.css") {
+      await clearBundle(output_file);
+    } else {
+      continue;
+    }
+  };
+
   const files = await readdir(source_folder, {withFileTypes: true});
   for (const file of files) {
     const file_path = join(source_folder, file.name);
     const extension = extname(file_path);
     if (file.isFile() === true && extension === ".css") {
-
+      await mergeSingleFile(file_path);
+      //console.log(`Merge file ${file_path}`);
     }
-  }
-} */
+  };
+}
 
-let new_path = join(__dirname, "styles", "style-1.css");
-
-copyFileContent(new_path);
+createBundle();
