b506bfd28b5209c4a8d61f6a9e0438d3ef25da35
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2024-01-22 14:38:00 +1300

    refactor: refactor the code to unify it with later task solutions

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,14 +1,15 @@
-const fs = require("fs");
-const path = require("path");
+const { createReadStream } = require("node:fs");
+const { join } = require("node:path");
+const { stdout } = require("node:process");
 
-const file_path = path.join(__dirname, "text.txt");
+const file_path = join(__dirname, "text.txt");
 const result_output = [];
-const read_stream = fs.createReadStream(file_path, "utf8");
+const read_stream = createReadStream(file_path, "utf8");
 
 read_stream.on("data", function(chunk) {
   result_output.push(chunk); 
 });
 
 read_stream.on("end", function() {
-  console.log(result_output.join(""));
+  stdout.write(result_output.join(""));
 });
